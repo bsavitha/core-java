e39308f265401995f97f185c25dc25f454db78b3
bsavitha <91309044+bsavitha@users.noreply.github.com>
2021-09-24 12:13:49 +0530

    Add files via upload

---
+++ Tooth.java
@@ -0,0 +1,9 @@
+class Tooth{
+public static void main(String[] values){
+Toothbrush ref=new Toothbrush();
+String ref1 = ref.color;
+System.out.println(ref1);
+
+
+}
+}
---
+++ test1.java
@@ -0,0 +1,15 @@
+class test1{
+	public static void main(String[] values){
+i=1;
+j=1;
+Repeat till (i<=3){
+Repeat till (j<=3){
+print(i);
+j=j+1;
+}
+i=i+1;
+print(j);
+j=1;
+}
+}
+}
